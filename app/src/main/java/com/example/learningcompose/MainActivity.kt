ada770a253e221f28e1ab486c252e3f2834a0787
vrexpert <vrexpert2302@gmail.com>
2023-12-24 17:08:41 +0900

    feat: 설정 완료

--- app/src/main/java/com/example/learningcompose/MainActivity.kt
+++ app/src/main/java/com/example/learningcompose/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.learningcompose
 
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -25,6 +26,8 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.learningcompose.ui.theme.LearningcomposeTheme
@@ -84,13 +87,18 @@ fun Greeting(name: String) {
                     .weight(1f)
                     .padding(bottom = extraPadding.coerceAtLeast(0.dp)),
             ) {
-                Text(text = "Hello,  ")
-                Text(text = "$name!")
+                Text(text = stringResource(R.string.hello))
+                Text(
+                    text = "$name!",
+                    style = MaterialTheme.typography.headlineMedium.copy(
+                        fontWeight = FontWeight.ExtraBold,
+                    ),
+                )
             }
             ElevatedButton(
                 onClick = { expended = !expended },
             ) {
-                Text(if (expended) "접기!" else "펼치기")
+                Text(if (expended) stringResource(R.string.show_less) else stringResource(R.string.show_more))
             }
         }
     }
@@ -121,11 +129,12 @@ fun MyAppPreview() {
     }
 }
 
+@Preview(showBackground = true, widthDp = 320, uiMode = UI_MODE_NIGHT_YES, name = "night")
 @Preview(showBackground = true, widthDp = 320)
 @Composable
 fun GreetingsPreview() {
     LearningcomposeTheme {
-        Greetings(names = listOf("최베르", "최성훈", "베르최", "최베르", "최성훈", "베르최", "최베르", "최성훈", "베르최"))
+        Greetings(names = listOf("최베르", "최성훈", "베르최", "최베르", "최성훈", "베르최"))
     }
 }
 
